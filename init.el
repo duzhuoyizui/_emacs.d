bf5d4a4f31407ce4bb52f14e49afe999a381bb8e
jerryzhang <chinajiezhang@gmail.com>
2017-01-06 10:36:07 +0800

    helm

--- init.el
+++ init.el
@@ -188,15 +188,15 @@ Uses `current-date-time-format' for the formatting the date/time."
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
 (global-set-key (kbd "C-c h o") 'helm-occur)
 
-;; (setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
-;;       helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
-;;       helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
-;;       helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
-;;       helm-ff-file-name-history-use-recentf t
-;;       helm-buffers-fuzzy-matching           t
-;;       helm-recentf-fuzzy-match              t
-;;       helm-M-x-fuzzy-match                  t
-;;       )
+(setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+      helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+      helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+      helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+      helm-ff-file-name-history-use-recentf t
+      helm-buffers-fuzzy-matching           t
+      helm-recentf-fuzzy-match              t
+      helm-M-x-fuzzy-match                  t
+      )
 
 (helm-mode 1)
 
