8d00d0a3af6d59687ba1ee1f5dc4826d062f785c
zhangjie <me@zhangjiee.com>
2018-06-21 20:19:06 +0800

    降低 emacs 需求版本限制

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 ;; -*- coding: utf-8 -*-
 (package-initialize)
 
-(let ((minver "25.3"))
+(let ((minver "25.1"))
   (when (version< emacs-version minver)
     (error "This config requires Emacs v%s or higher" minver)))
 
