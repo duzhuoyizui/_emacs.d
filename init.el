25ea99f6885af341ce3dc6d2ff8a3a8225ed434a
zhangjie <me@zhangjiee.com>
2019-01-29 15:07:45 +0800

    update

--- init.el
+++ init.el
@@ -24,7 +24,10 @@
   (require-init 'init-go)
   (require-init 'init-highlight-symbol)
   (require-init 'init-ivy)
+<<<<<<< HEAD
   (require-init 'init-git)
+=======
+>>>>>>> 8821655... remove keyfreq
   (require-init 'init-python)
   (require-init 'init-ui)
   (require-init 'init-web)
