7a73ee08f62b9256541826f11c89295fbb21ff09
zhangjie <me@zhangjiee.com>
2019-05-28 15:50:27 +0800

    gui setting

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -1,7 +1,22 @@
 ;; 关闭启动消息
 (setq inhibit-startup-message t)
+(setq inhibit-startup-echo-area-message t)
+(setq use-file-dialog nil)
+(setq use-dialog-box nil)
+(blink-cursor-mode 0)
 
-(menu-bar-mode -1)
+;; Show a marker in the left fringe for lines not in the buffer
+(setq indicate-empty-lines t)
+
+;; NO tool bar
+(if (fboundp 'tool-bar-mode)
+    (tool-bar-mode -1))
+;; no scroll bar
+(if (fboundp 'set-scroll-bar-mode)
+    (set-scroll-bar-mode nil))
+;; no menu bar
+(if (fboundp 'menu-bar-mode)
+    (menu-bar-mode -1))
 
 (column-number-mode t) ; show column number
 
@@ -10,5 +25,12 @@
 (load-theme 'leuven)
 
 (add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+
+;; Set default font
+(set-face-attribute 'default nil
+                    :family "Fira code"
+                    :height 130
+                    :weight 'normal
+                    :width 'normal)
 
 (provide 'init-ui)
