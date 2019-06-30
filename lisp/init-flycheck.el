4894d866118daacbb77f8c04147bff987bffe8d9
JerryZhang <me@zhangjiee.com>
2019-06-30 12:28:07 +0800

    flycheck compatible

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -6,7 +6,14 @@
   (setq-default flycheck-disabled-checkers
                 (append flycheck-disabled-checkers
                         '(javascript-jshint)))
-  (setq flycheck-javascript-eslint-executable "eslint")
+
+  (cond
+   ((string-equal system-type "gnu/linux")
+    (setq flycheck-javascript-eslint-executable "/home/jerryzhang/qwerty-client/src/node_modules/eslint/bin/eslint.js")
+    )
+   ((string-equal system-type "darwin")
+    (setq flycheck-javascript-eslint-executable "eslint")
+    ))
   (setq flycheck-python-flake8-executable "flake8")
   (global-flycheck-mode)
   :config
