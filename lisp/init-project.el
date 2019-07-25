6be9c2164abe44fb21fd166a61d6ae29d3170194
JerryZhang <me@zhangjiee.com>
2019-07-25 22:04:27 +0800

    commit git

--- lisp/init-project.el
+++ lisp/init-project.el
@@ -39,10 +39,10 @@
   (counsel-projectile-mode 1)
   )
 
-(use-package magit
-  :ensure t
-  :defer t
-  :bind (("C-x g" . magit-status)))
+;; (use-package magit
+;;   :ensure t
+;;   :defer t
+;;   :bind (("C-x g" . magit-status)))
 
 (use-package avy
   :ensure t
