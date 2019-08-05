585b86886cab4788af2704e0062ce66ab4909c1c
zhangjie <me@zhangjiee.com>
2019-08-05 14:38:36 +0800

    allow C-h to trigger which-key before it is done automatically

--- lisp/init-which-key.el
+++ lisp/init-which-key.el
@@ -2,7 +2,11 @@
   :ensure t
   :init
   :config
-  (which-key-setup-minibuffer)
+  ;; Allow C-h to trigger which-key before it is done automatically
+  (setq which-key-show-early-on-C-h t)
+  (setq which-key-idle-delay 10000)
+  (setq which-key-idle-secondary-delay 0.05)
+  (which-key-setup-side-window-right)
   (which-key-mode)
   )
 
