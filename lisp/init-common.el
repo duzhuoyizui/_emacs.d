48b6b7dd689461c6e3583f402448a445468d0503
zhangjie <me@zhangjiee.com>
2020-01-16 17:02:11 +0800

    重新定义日期格式

--- lisp/init-common.el
+++ lisp/init-common.el
@@ -14,26 +14,15 @@
 (electric-indent-mode 1)
 ;;(electric-quote-mode 1)
 
-(defvar current-date-time-format "%Y-%m-%d %H:%M:%S"
+(defvar current-date-time-format "%Y-%m-%d %H:%M:%S %a"
   "Format of date to insert with `insert-current-date-time' func
 See help of `format-time-string' for possible replacements")
-
-(defvar current-time-format "%a %H:%M:%S"
-  "Format of date to insert with `insert-current-time' func.
-Note the weekly scope of the command's precision.")
 
 (defun insert-current-date-time ()
   "insert the current date and time into current buffer.
 Uses `current-date-time-format' for the formatting the date/time."
   (interactive)
   (insert (format-time-string current-date-time-format (current-time)))
-  )
-
-(defun insert-current-time ()
-  "insert the current time (1-week scope) into the current buffer."
-  (interactive)
-  (insert (format-time-string current-time-format (current-time)))
-  (insert "\n")
   )
 
 (defun show-file-name ()
@@ -103,7 +92,6 @@ Uses `current-date-time-format' for the formatting the date/time."
   (global-set-key (kbd "C-c k") 'counsel-ag)
   (global-set-key (kbd "M-y") 'counsel-yank-pop)
   )
-
 
 (use-package projectile
   :ensure t
