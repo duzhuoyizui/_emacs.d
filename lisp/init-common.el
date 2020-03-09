de67e6d9b3a09bcbb0bbf0c02602b357661fc5e7
zhangjie <me@zhangjiee.com>
2020-03-09 18:14:59 +0800

    add multi/edit current in fun

--- lisp/init-common.el
+++ lisp/init-common.el
@@ -38,11 +38,10 @@ Uses `current-date-time-format' for the formatting the date/time."
   :pin melpa
   :ensure t
   :config
+  (global-set-key (kbd "M-s ;") 'mc/mark-all-like-this-in-defun)
   (global-set-key (kbd "M-s >") 'mc/mark-next-like-this)
   (global-set-key (kbd "M-s <") 'mc/mark-previous-like-this)
   (global-set-key (kbd "M-s C-a") 'mc/mark-all-like-this)
-  (global-set-key (kbd "M-s a") 'mc/edit-beginnings-of-lines)
-  (global-set-key (kbd "M-s e") 'mc/edit-ends-of-lines)
   )
 
 ;; https://github.com/magnars/expand-region.el
