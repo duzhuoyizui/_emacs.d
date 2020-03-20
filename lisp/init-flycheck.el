cd0557ea6752331e7fa82d229dbbbe5cd542479f
zhangjie <me@zhangjiee.com>
2020-03-20 11:10:35 +0800

    disable global flycheck

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -1,12 +1,14 @@
 (use-package flycheck
   :pin melpa
   :ensure t
+  :defer t
   :init
   (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc))
   (setq-default flycheck-disabled-checkers
                 (append flycheck-disabled-checkers
                         '(javascript-jshint)))
 
+  ;; set flycheck tool
   (cond
    ((string-equal system-type "gnu/linux")
     (setq flycheck-javascript-eslint-executable "/home/jerryzhang/qwerty-client/src/node_modules/eslint/bin/eslint.js")
@@ -15,7 +17,11 @@
     (setq flycheck-javascript-eslint-executable "eslint")
     ))
   (setq flycheck-python-flake8-executable "flake8")
-  (global-flycheck-mode)
+
+  (setq flycheck-check-syntax-automatically '(mode-enabled save))
+
+  ;; 手动检查
+  (global-set-key (kbd "M-s m") 'flycheck-mode)
 
   ;;:config
   ;;(flycheck-add-mode 'javascript-eslint 'web-mode)
