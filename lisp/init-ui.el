ee8fac0a3c361bdaf683db681ed2d1749f2beb51
zhangjie <me@zhangjiee.com>
2020-03-26 15:05:26 +0800

    delete dashboard

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -25,21 +25,6 @@
 
 (load-theme 'leuven)
 
-(use-package dashboard
-  :pin melpa
-  :ensure t
-  :config
-  (setq dashboard-banner-logo-title "Welcome to Emacs")
-  (setq dashboard-init-info "the truth is what it is, not what you see")
-  (setq dashboard-center-content t)
-  (setq dashboard-set-init-info t)
-  (setq dashboard-set-footer t)
-  (setq dashboard-set-navigator t)
-  (setq dashboard-items '((recents . 8)
-                        (projects . 5)))
-  (dashboard-setup-startup-hook)
-  )
-
 ;; 需要安装 all-the-icons: =M-x all-the-icons-install-fonts=
 (use-package doom-modeline
   :pin melpa
