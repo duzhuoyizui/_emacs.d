8e129e546bb9707dee4871b8c9947d3f18022f3a
zhangjie <me@zhangjiee.com>
2020-03-27 12:12:54 +0800

    rebind

--- lisp/init-hydra.el
+++ lisp/init-hydra.el
@@ -17,17 +17,15 @@
 							  :foreign-keys warn
 							  :timeout 1
 							  :idle 60
+							  :hint nil
+							  :exit t
 							  )
 	"bydra zhj-key"
-	;; basic move
-	("k" previous-line "prev line")
-	("j" next-line "next line")
-	("l" forward-char "forward char")
-	("h" backward-char "backward char")
+	;;
 	("a" move-beginning-of-line "move beginning of line")
 	("e" move-end-of-line "move end of line")
-	("[" beginning-of-buffer "beginning of buffer")
-	("]" end-of-buffer "end of buffer")
+	("," beginning-of-buffer "beginning of buffer")
+	("." end-of-buffer "end of buffer")
 
 	;; search
 	("g" counsel-ag "ag search")
@@ -41,11 +39,9 @@
 	("w" save-buffer "save buffer")
 	("s" swiper "search")
 
-	("h" (hydra-set-property 'hydra-toggle :verbosity 1) :exit nil)
 	("q" nil "quit")
 	)
   (global-set-key (kbd "<f8>") 'hydra-zhj-key/body)
   )
-
 
 (provide 'init-hydra)
