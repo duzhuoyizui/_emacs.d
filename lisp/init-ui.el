de702c7ff840572dc5153a0f7559f4e231a01663
JerryZhang <me@zhangjiee.com>
2021-01-11 10:50:16 +0800

    shit

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -61,21 +61,20 @@
   :hook (prog-mode-hook . rainbow-delimiters-mode)
   )
 
-(use-package zenburn-theme
-  :pin melpa
-  :ensure t
-  :config
-  ;; use variable-pitch fonts for some headings and titles
-  (setq zenburn-use-variable-pitch t)
-  ;; scale headings in org-mode
-  (setq zenburn-scale-org-headlines t)
-  ;; scale headings in outline-mode
-  (setq zenburn-scale-outline-headlines t)
-  (load-theme 'zenburn t)
-  )
+;; (use-package zenburn-theme
+;;   :pin melpa
+;;   :ensure t
+;;   :config
+;;   ;; use variable-pitch fonts for some headings and titles
+;;   (setq zenburn-use-variable-pitch t)
+;;   ;; scale headings in org-mode
+;;   (setq zenburn-scale-org-headlines t)
+;;   ;; scale headings in outline-mode
+;;   (setq zenburn-scale-outline-headlines t)
+;;   (load-theme 'zenburn t)
+;;   )
 
-;; (load-theme 'leuven)
-
+(load-theme 'leuven)
 
 (set-face-foreground 'line-number "darkgrey")
 
