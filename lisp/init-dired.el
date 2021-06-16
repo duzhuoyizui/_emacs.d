ce6fafb6048457265dfb99643a201bdae70a8136
JerryZhang <me@zhangjiee.com>
2021-06-16 10:02:50 +0800

    set dired-subtree background

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -1,9 +1,12 @@
 (use-package dired-subtree
   :pin melpa
   :ensure t
+  :init
+  (setq dired-subtree-use-backgrounds nil)
   :config
   (bind-keys :map dired-mode-map
              ("i" . dired-subtree-insert)
-             (";" . dired-subtree-remove)))
+             (";" . dired-subtree-remove))
+  )
 
 (provide 'init-dired)
